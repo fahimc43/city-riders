074675e8f0e0a362f8001c66aba048962db702ab
Fahim Chowdhury <fahimc43@gmail.com>
2021-07-08 17:19:33 +0600

    sign out condition

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import './Header.css';
 
 const Header = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
@@ -15,17 +16,22 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ml-lg-auto">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+                            <Link className="nav-link" aria-current="page" to="/home">Home</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/destination">Destination</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
+                            {loggedInUser.email? <Link className="nav-link" onClick={() => setLoggedInUser({})}>Sign Out</Link>
+                            : <Link className="nav-link" to="/login">Login</Link>
+                            }
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/home">{loggedInUser.name}</Link>
+                            <Link className="nav-link active user-email" to="/home">{loggedInUser.email}</Link>
                         </li>
+                        {/* <li className="nav-item">
+                            <button className="nav-button">Sign Out</button>
+                        </li> */}
                     </ul>
                 </div>
             </div>
